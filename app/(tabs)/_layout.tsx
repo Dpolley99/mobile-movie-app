f010d17fd3448e7098aa44cf4192d5f066240cf3
Debarshi Polley <debarshipolley99@gmail.com>
2025-12-23 22:38:50 +0000

    TabIcon own component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,20 @@ import React from "react";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 
+const TabIcon = () => {
+  return (
+    <ImageBackground
+      source={images.highlight}
+      className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+    >
+      <Image
+        source={icons.home} tintColor="#151312" className='size-5'
+      />
+      <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
+    </ImageBackground>
+  )
+}
+
 const _layout = () => {
   return (
     <Tabs>
@@ -13,17 +27,7 @@ const _layout = () => {
           title: 'Home',
           headerShown: false, 
           tabBarIcon: ({ focused }) => (
-            <>
-              <ImageBackground
-                source={images.highlight}
-                className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
-              >
-                <Image
-                  source={icons.home} tintColor="#151312" className='size-5'
-                />
-                <Text className="p-2">Home</Text>
-              </ImageBackground>
-            </>
+            <TabIcon />
           )
         }}
       />
